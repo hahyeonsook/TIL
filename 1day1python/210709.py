236a43bba26229dfa5e41b4bbcd6a4436aad95bd
hyeonsook95 <qu3230@gmail.com>
2021-07-09 18:31:27 +0900

    210709 1day1python

--- 1day1python/210709.py
+++ 1day1python/210709.py
@@ -1,19 +1,40 @@
-# REF https://www.30secondsofcode.org/python/s/check-prop
-# 주어진 객체의 속성을 주어진 함수에 적용하는 함수를 생성하라.
-def check_prop(fn, prop):
-    return lambda obj: fn(obj[prop])
+# REF https://www.30secondsofcode.org/python/s/have-same-contents
+# 순서와 관계 없이 두 리스트에 동일한 요소가 포함되어 있는지 확인하라.
+def have_same_contents(a, b):
+    # set은 집합으로 원소의 중복을 없앰.
+    for v in set(a + b):
+        # count() : 찾고자 하는 항목이 리스트에 몇 개 들었는지 확인
+        if a.count(v) != b.count(v):
+            return False
+    return True
 
 
-check_age = check_prop(lambda x: x >= 18, "age")
-user = {"name": "Mark", "age": 18}
-print(check_age(user))  # True
+print(have_same_contents([1, 2, 4], [2, 4, 1]))  # True
+print(have_same_contents([1, 2, 4, 4], [2, 4, 1]))  # False
 
-# REF https://www.30secondsofcode.org/python/s/none
-# 주어진 함수가 리스트의 요소에 대해 하나 이상이 True를 반환하는지 확인하라.
-def none(lst, fn=lambda x: x):
-    # all() :  모든 요소가 참이거나 비어있으면 True, 그 외의 경우에는 False
-    return all(not fn(x) for x in lst)
+# REF https://www.30secondsofcode.org/python/s/gcd
+# 숫자들의 리스트의 최대공약수를 구하라.
+from functools import reduce
+from math import gcd as _gcd
 
 
-print(none([0, 1, 2, 0], lambda x: x >= 2))  # False
-print(none([0, 0, 0]))  # True
+def gcd(numbers):
+    # reduce() : iterable한 변수를 fn 누적 적용하여 단일 값으로 리턴하는 함수
+    # math.gcd() : python 3.5 최대공약수를 구하는 함수
+    # recude(_gcd, numbers) = _gcd(_gcd(_gcd(numbers[0]), numbers[1]), numbers[2])
+    return reduce(_gcd, numbers)
+
+
+print(gcd([8, 36, 28]))  # 4
+
+# REF https://www.30secondsofcode.org/python/s/to-iso-date
+# ISO-8601 https://ko.wikipedia.org/wiki/ISO_8601
+# 날짜를 ISO-8601 표기로 변환하라.
+from datetime import datetime
+
+
+def to_iso_date(d):
+    return d.isoformat()
+
+
+print(to_iso_date(datetime(2021, 7, 9)))  # 2021-07-09T00:00:00
