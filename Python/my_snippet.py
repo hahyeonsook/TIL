30397dfc90986b255d22adcecef203d029ce3d8c
hyeonsook95 <qu3230@gmail.com>
2021-07-10 02:44:39 +0900

    추가

--- Python/my_snippet.py
+++ Python/my_snippet.py
@@ -53,6 +53,8 @@ def iterative_dfs(start_v: int) -> list[int]:
     stack = [start_v]
     while stack:
         v = stack.pop()
+        # 루프 문이 시작되기 전에 확인했다고 표시했으므로,
+        # 루프 문에서 볼 자식 노드의 부모 노드만 확인했다고 표시한 것.
         if v not in discovered:
             discovered.append(v)
             for w in graph[v]:
@@ -73,6 +75,8 @@ def iterative_bfs(start_v):
         v = queue.pop(0)
         # v의 모든 자식 노드를 순회
         for w in graph[v]:
+            # 루프 문이 시작되고 자식 노드를 queue에 넣을 때 확인했음을 표시했으므로,
+            # 한 부모 노드의 모든 자식 노드를 확인했다고 표시한 것.
             if w not in discovered:
                 discovered.append(w)
                 queue.append(w)
@@ -118,3 +122,57 @@ from math import gcd as _gcd
 
 def gcd(numbers):
     return reduce(_gcd, numbers)
+
+
+# 최대공약수
+# 유클리드 알고리즘
+
+# 1. a, b(a > b)
+# 2. b == 0, return a
+# 3. a % b != 0, a = b b = a % b, 1로 다시 돌아감
+# 4. a % b == 0, return b
+
+
+def _gcd(a, b):
+    a, b = a, b if a > b else b, a
+    if b == 0:
+        return a
+    if a % b == 0:
+        return b
+    else:
+        return gcd(b, a % b)
+
+
+# 문자열이 아닌 리스트 join으로 출력하기
+lst = [1, 2, 3, 4]
+print(" ".join(map(str, lst)))
+
+
+# 조합 Combination
+# nCm, 순서를 고려하지 않고 n 개중에서 m개를 늘어놓음.
+# nCm = nPm/m! = n!/r!(n-r)!
+# https://juhee-maeng.tistory.com/91
+from itertools import combinations
+
+for i in combinations([1, 2, 3, 4], 2):
+    print(i, end=" ")
+
+# 조합
+# https://m.blog.naver.com/PostView.nhn?blogId=kmh03214&logNo=221685090465&targetKeyword=&targetRecommendationCode=1
+def combinations(arr, r):
+    for i in range(len(arr)):
+        if r == 1:
+            yield [arr[1]]
+        for next in combinations(arr[i + 1], r - 1):
+            yield [arr[i] + next]
+
+
+# 소수 구하기
+import math
+
+
+def isPrime(number):
+    for i in range(2, int(math.sqrt(number)) + 1):
+        if number % i == 0:
+            return False
+    return True
