52dc0a535f65a48a52ac4e45ca4d5b8e315e8fa0
hyeonsook95 <qu3230@gmail.com>
2021-10-06 22:24:39 +0900

    211006 문제풀기

--- Algorithms/Practice/backjoon/1092.py
+++ Algorithms/Practice/backjoon/1092.py
@@ -33,6 +33,8 @@ def solution():
     for box in boxes:
         idx = 0
         for j in range(N):
+            # 비어있는 상자에 넣는게 최소값을 구할 수 있는 경우이므로,
+            # j에 들어있는 화물이 적으면 j에 화물을 넣는거임
             if box <= cranes[j] and cnt[idx] > cnt[j]:
                 idx = j
             elif box > cranes[j]:
